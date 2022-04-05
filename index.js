980993362b949984c3e9cb2d409821f0dd4e59f0
Rachel Katz <rachel.katz@flatironschool.com>
2022-04-05 14:41:33 -0400

    commit tree c4aeaf92cc7efedb664cae355950c92da45d875e

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+let findMatching = (drivers, driver) => {
+    return drivers.filter(el => el.toLowerCase() === driver.toLowerCase())
+}
+
+let fuzzyMatch = (drivers, driver) => {
+    return drivers.filter(el => el.slice(0, driver.length) === driver)
+}
+
+let matchName = (drivers, driver) => {
+    let matchArray = [];
+    for (let i in drivers) {
+        if (drivers[i].name === driver) {
+            matchArray.push(drivers[i])
+        }
+    }
+    return drivers.filter(el => el.name === driver)
+}
