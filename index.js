716ee698e7f399512c8f61768e2b0bd9989df89c
Rachel Katz <rachel.katz@flatironschool.com>
2022-04-05 14:41:47 -0400

    my solution

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+let findMatching = (drivers, driver) => {
+    return drivers.filter(el => el.toLowerCase() === driver.toLowerCase())
+}
+
+let fuzzyMatch = (drivers, driver) => {
+    return drivers.filter(el => el.slice(0, driver.length) === driver)
+}
+
+let matchName = (drivers, driver) => {
+    return drivers.filter(el => el.name === driver)
+}
